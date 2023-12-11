3c0a71dc1dd1f6c2bdcf456a2ac5d040084def87
bbcvasconcellos <bbcvasconcellos@gmail.com>
2023-12-11 06:48:14 -0300

    fix: moduleCard css

--- src/components/moduleCard.tsx
+++ src/components/moduleCard.tsx
@@ -9,12 +9,12 @@ interface ModuleCardProps {
 
 export const ModuleCard = ({ title, description, buttonText, buttonURL }: ModuleCardProps) => {
   return (
-    <div className="grid grid-rows-2 bg-bgModulesCard p-5 rounded-2xl gap-10 max-h-44 min-w-[280px] max-w-[397px] w-fit mr-6">
+    <div className="grid grid-rows-2 bg-bgModulesCard p-5 pb-4 rounded-2xl gap-10 max-h-44 min-w-[280px] max-w-[397px] w-fit mr-6">
       <div>
         <h1 className="text-xl font-medium mb-2">{title}</h1>
         <p className="text-support-text-module">{description}</p>
       </div>
-      <Link href={buttonURL} className="flex items-center text-support-text-blue gap-5">
+      <Link href={buttonURL} className="flex items-end text-support-text-blue gap-5">
         {buttonText}
         <ArrowUpRight size={24}/>
       </Link>
